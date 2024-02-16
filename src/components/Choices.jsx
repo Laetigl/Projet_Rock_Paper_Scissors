dd8cf9038494045e54ae1ec5e67194ef4400b1bf
Laetitia Gallegos Lopez <laetitiagallegoslopez@MacBook-Air-de-Laetitia.local>
2024-02-16 10:24:42 +0100

    github page

--- src/components/Choices.jsx
+++ src/components/Choices.jsx
@@ -14,11 +14,11 @@ export default function Choices(props) {
     <div className='flex justify-center items-center'>
         <div className="triangleBg flex flex-col justify-center items-center gap-[50px]">
             <div className='topHand flex gap-[60px] '>
-                <button onClick={() => {props.setUserChoice("paper");props.setPickHouse(randomImg())}} className='borderCirclePaper rounded-[50%] p-[30px] bg-white lg:w-[200px] lg:h-[200px] lg:flex lg:justify-center lg:items-center '> <img className='iconImg lg:w-[80px] lg:h-[100px]' src={paper} alt="paper" /></button>
-                <button onClick={() => {props.setUserChoice("scissors");props.setPickHouse(randomImg())}} className='borderCircleScissors rounded-[50%] p-[30px] bg-white lg:w-[200px] lg:h-[200px] lg:flex lg:justify-center lg:items-center '> <img className='iconImg lg:w-[80px] lg:h-[100px]' src={scissors} alt="scissors" /></button>
+                <button onClick={() => {props.setUserChoice("paper");props.setPickHouse(randomImg())}} className='borderCirclePaper rounded-[50%] p-[30px] bg-white lg:w-[200px] lg:h-[200px] lg:flex lg:justify-center lg:items-center '> <img className='iconImg w-[80px] h-[100px] lg:w-[80px] lg:h-[100px]' src={paper} alt="paper" /></button>
+                <button onClick={() => {props.setUserChoice("scissors");props.setPickHouse(randomImg())}} className='borderCircleScissors rounded-[50%] p-[30px] bg-white lg:w-[200px] lg:h-[200px] lg:flex lg:justify-center lg:items-center '> <img className='iconImg w-[80px] h-[100px] lg:w-[80px] lg:h-[100px]' src={scissors} alt="scissors" /></button>
             </div>
             <div className='"bottomHand'>
-                <button onClick={() => {props.setUserChoice("rock");props.setPickHouse(randomImg())}} className='borderCircleRock rounded-[50%] p-[30px] bg-white lg:w-[200px] lg:h-[200px] lg:flex lg:justify-center lg:items-center '> <img className='iconImg lg:w-[80px] lg:h-[100px]' src={rock} alt="" /></button>
+                <button onClick={() => {props.setUserChoice("rock");props.setPickHouse(randomImg())}} className='borderCircleRock rounded-[50%] p-[30px] bg-white w-[200px] h-[200px] lg:w-[200px] lg:h-[200px] lg:flex lg:justify-center lg:items-center '> <img className='iconImg w-[80px] h-[100px] lg:w-[80px] lg:h-[100px]' src={rock} alt="" /></button>
             </div>
         </div>
     </div>
--- src/components/Restart.jsx
+++ src/components/Restart.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Restart(props) {
   return (
-    <div className="rounded-[20px] flex flex-col justify-center items-center  w-[200px] h-[50px] bg-white gap-[20px]">
-        <button onClick={()=> {props.setUserChoice("choice") ; props.setPickHouse ("");props.counter()}} className='text-[20px] text-black'>PLAY AGAIN</button>
+    <div className="rounded-[20px] flex flex-col justify-center items-center  w-[200px] h-[50px] bg-white gap-[20px] hover:bg-[#494b7a] ">
+        <button onClick={()=> {props.setUserChoice("choice") ; props.setPickHouse ("");props.counter()}} className='text-[20px] text-black  hover:text-white hover:font-bold' >PLAY AGAIN</button>
     </div>
   )
 }
